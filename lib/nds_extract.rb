1e72fc4cb36a98dbd2ecf85f1546731aa4800612
Grant Ferowich <g.ferowich@gmail.com>
2019-10-24 18:13:55 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -1,22 +1,28 @@
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'directors_database'
+require 'pp'
 
 def directors_totals(nds)
-  # Remember, it's always OK to pretty print what you get *in* to make sure
-  # that you know what you're starting with!
-  #
-  #
-  # The Hash result be full of things like "Jean-Pierre Jeunet" => "222312123123"
+
   result = {
   }
-  #
-  # Use loops, variables and the accessing method, [], to loop through the NDS
-  # and total up all the
-  # ...
-  # ...
-  # ...
-  #
-  #
-  # Be sure to return the result at the end!
-  nil
+  
+  i = 0 #which movie director's hash we're on
+  
+  while i < nds.length do
+    tranch = 0 #which movie director we're on
+   j = 0
+    while j < nds[:movies].length do
+     total += nds[:movies][:worldwide_gross].to_i 
+     j += 1 
+    end
+  
+    result[name] = total
+    i += 1 
+
+  end  
+  
+  return result
 end
+
+#NDS is set up as an array of hashes; the first key in each hash is the director's 'name', and the second key points to the director's 'movies'. The 'movies' key points to another array of hashes: inside each of the sub-hashes inside 'movies' is a collection of data points that model a movie - title, studio, worldwide_gross, and release year. 
